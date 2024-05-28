a4f4b182e76e02e36820bba996235c8b8059bd10
Keishiro308 <kn11308@gmail.com>
2024-05-28 22:50:12 +0900

    actionについて

--- stories/StyledButton.stories.tsx
+++ stories/StyledButton.stories.tsx
@@ -1,15 +1,25 @@
 import { Meta } from '@storybook/react';
 import { StyledButton } from '../components/StyledButton';
+import { action } from '@storybook/addon-actions';
+import React, { useState } from 'react';
 
 export default {
   title: 'StyledButton',
   component: StyledButton,
+  // argTypes: { onClick: { action: 'clicked' } },
 } as Meta<typeof StyledButton>;
 
+const incrementAction = action('increment');
+
 export const Primary = (props: any) => {
+  const [count, setCount] = useState(0);
+  const onClick = (e: React.MouseEvent) => {
+    incrementAction(e, count);
+    setCount(count + 1);
+  }
   return (
-    <StyledButton variant="primary" {...props}>
-      Primary
+    <StyledButton variant="primary" {...props} onClick={onClick}>
+      Count: {count}
     </StyledButton>
   )
 }
